d3bc8d918ba5fd5ed6c4d5c04343b6d11646cd5b
Kevin Sarria <reysabueso256@gmail.com>
2022-05-02 17:28:04 -0500

    Tercer proyecto terminado

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,8 +1,17 @@
 'use strict'
 
 // Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+// Contener para los resultados
+const resultado = document.querySelector('#resultado');
+
 
 
 const max = new Date().getFullYear();
@@ -24,17 +33,57 @@ const datosBusqueda = {
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-    mostrarAutos(); // Muestra los automoviles al cargar
+    mostrarAutos(autos); // Muestra los automoviles al cargar
 
     // Llena las opciones de años
     llenarSelect();
 
 });
 
+// Event Listeners para los select de busqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+});
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value;
+    filtrarAuto();
+});
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value;
+    filtrarAuto();
+});
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = parseInt(e.target.value);
+    filtrarAuto();
+});
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value;
+    filtrarAuto();
+});
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value;
+    filtrarAuto();
+});
+
+
 
 
 // Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+
+    limpiarHTML(); // Elimina el HTML previo
+
     autos.forEach(auto => {
 
         const {marca,modelo,year,puertas,transmision,precio,color} = auto;
@@ -49,6 +98,14 @@ function mostrarAutos() {
     })
 }
 
+// Limpiar el HTML
+function limpiarHTML() {
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+
 
 // Genera los años del select
 function llenarSelect() {
@@ -61,16 +118,84 @@ function llenarSelect() {
 }
 
 
+// Funcion que filtra en base a la busqueda
+function filtrarAuto() {
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransimision).filter(filtrarColor);
+
+    if(resultado.length) {
+        mostrarAutos(resultado);
+    }else {
+        noResultado();
+    }
+
+}
 
 
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if(marca) {
+        return auto.marca === marca;
+    }
+    return auto;
+}
 
+function filtrarYear(auto){
+    const {year} = datosBusqueda;
+    if(year) {
+        return auto.year === year;
+    }
+    return auto;
+}
 
+function filtrarMinimo(auto){
+    const {minimo} = datosBusqueda;
+    if(minimo) {
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
 
+function filtrarMaximo(auto){
+    const {maximo} = datosBusqueda;
+    if(maximo) {
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
 
+function filtrarPuertas(auto){
+    const {puertas} = datosBusqueda;
+    if(puertas) {
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
 
+function filtrarTransimision(auto){
+    const {transmision} = datosBusqueda;
+    if(transmision) {
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
 
+function filtrarColor(auto){
+    const {color} = datosBusqueda;
+    if(color) {
+        return auto.color === color;
+    }
+    return auto;
+}
 
+function noResultado() {
 
+    limpiarHTML();
+
+    const noResultado = document.createElement('div');
+    noResultado.classList.add('alerta', 'error');
+    noResultado.textContent = 'No Hay Resultados, Intenta Con Otros Terminos De Busqueda.';
+    resultado.appendChild(noResultado);
+}
 
 
 
