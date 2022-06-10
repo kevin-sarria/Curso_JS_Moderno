c9107ccc44773c9da8cb075ffec5ee2ffd1b8c3e
Kevin Sarria <reysabueso256@gmail.com>
2022-06-09 20:33:08 -0500

    avances curso js moderno

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -1,10 +1,32 @@
 
 
+// Variables
+
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+
+// Event Listeners
+
+btnGuardarCliente.addEventListener('click', guardarCliente);
 
 
+function guardarCliente() {
+    const mesa = document.querySelector('#mesa').value;
+    const hora = document.querySelector('#hora').value;
+
+    // Revisar si hay campos vacios
+    if( mesa === '' || hora === '' ) {
+        
+        return;
+    }
 
 
-
+}
 
 
 
