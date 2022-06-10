4f672bf1dc791c1b31b4381e987b7d717a4f8725
Kevin Sarria <reysabueso256@gmail.com>
2022-06-09 21:38:24 -0500

    avances curso js

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -20,10 +20,29 @@ function guardarCliente() {
     const hora = document.querySelector('#hora').value;
 
     // Revisar si hay campos vacios
-    if( mesa === '' || hora === '' ) {
+    const camposVacios = [ mesa, hora ].some( campo => campo === '' );
+
+    if(camposVacios) {
         
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta) {
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+            alerta.textContent = 'Todos los campos son obligatorios';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+
+        }
+
         return;
+
     }
+
+    
 
 
 }
