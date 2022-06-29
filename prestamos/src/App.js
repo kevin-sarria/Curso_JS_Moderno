d153a9a02de086046d104ad2454a211cbfd84ac5
Kevin Sarria <reysabueso256@gmail.com>
2022-06-29 16:13:24 -0500

    Comenzo la seccion de React

---
+++ prestamos/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import Header from './componentes/Header';
+
+
+function App() {
+  return (
+    <Fragment>
+        <Header 
+            titulo="Cotizador de prestamos"
+            
+        />
+
+    </Fragment>    
+  );
+}
+
+export default App;
---
+++ prestamos/src/componentes/Header.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+
+
+
+const Header = ({titulo}) => ( <h1>{titulo}</h1>);
+
+
+export default Header;
