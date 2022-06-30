fcaf663ce7ef125b4f50cb4958c4c68a3d0901f3
Kevin Sarria <reysabueso256@gmail.com>
2022-06-29 20:57:03 -0500

    Avances curso js moderno, apartado de React

--- prestamos/src/App.js
+++ prestamos/src/App.js
@@ -1,14 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './componentes/Header';
-
+import Formulario from './componentes/formulario';
 
 function App() {
+
+  // Definir el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [plazo, guardarPlazo] = useState(0);
+
+
   return (
     <Fragment>
         <Header 
             titulo="Cotizador de prestamos"
-            
         />
+
+        <div className="container">
+            <Formulario
+            cantidad={cantidad}
+            guardarCantidad={guardarCantidad}
+            plazo={plazo}
+            guardarPlazo={guardarPlazo}
+            />
+        </div>
 
     </Fragment>    
   );
